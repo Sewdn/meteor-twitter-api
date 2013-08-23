490ff57ae2fd463e837c30b7c4c40ebb3068f43a
Matthew Forr <matthew.forr@gmail.com>
2013-08-23 13:40:08 -0400

    Fixes for 0.6.5 changes.

--- package.js
+++ package.js
@@ -3,13 +3,12 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use([
-    'accounts-twitter'
-  ], 'server');
+  api.use('accounts-twitter', 'server');
+  api.use('oauth1', 'server');
+
+  api.export && api.export('TwitterApi', 'server');
 
   api.add_files(['lib/twitter.js'], 'server');
-
-  api.export('Twitter');
 });
 
 Package.on_test(function (api) {
